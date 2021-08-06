634e0bd15b6501f46530b972c47c7d847e59cc33
fa0311 <yukinashi2742756@yahoo.co.jp>
2021-08-06 14:48:02 +0900

    update menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,14 @@
 window.onload = function() {
-    let menu = {
-        twitter: "https://twitter.com/starprivate55",
-        instagram: "https://instagram.com/starprivate55",
-        youtube: "https://youtube.com/starprivate55",
-        tgrgroup: "https://tgrgroup.jp",
-    };
-
-
-    Object.keys(menu).forEach(key => {
-        console.log('key:' + key + ' value:' + menu[key]);
-        document.getElementById(key).onclick = function() {
-            location.href = menu[key];
-        };
-    });
-
-
     document.getElementById("waa").textContent = function() {
         let output = "";
         for (let i = 0; i < 10000; i++)
             output += "わー"
         return output
     }();
+
+    document.getElementById("menu-btn").onclick = function() {
+        this.classList.toggle('open');
+        this.classList.toggle('close');
+    }
+
 }
